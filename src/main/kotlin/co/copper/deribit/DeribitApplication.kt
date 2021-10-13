12f260f8cc26e76ca9d7386e28235a028c33e2d0
Andrejs Kirsanovs <adder@mpe.lv>
2021-10-13 19:19:40 +0100

    initial commit

---
+++ src/main/kotlin/co/copper/deribit/DeribitApplication.kt
@@ -0,0 +1,11 @@
+package co.copper.deribit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeribitApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeribitApplication>(*args)
+}
---
+++ src/test/kotlin/co/copper/deribit/DeribitApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.copper.deribit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeribitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
